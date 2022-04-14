e282a235e1b57ea2ede2b25743d7c8e32f2d203b
Seynabou12 <dioneseynabou0@gmail.com>
2022-04-14 01:03:13 +0000

    la différence entre deux nombres entiers

---
+++ Atelier2/Programme3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Programme3 {
+
+    public static void main(String[] args){
+
+        int v, w , t ;
+
+        Scanner b = new Scanner (System.in);
+
+        System.out.println("Entrer un entier v");
+        v = b.nextInt();
+
+        System.out.println("Entrer un entier w");
+        w = b.nextInt();
+
+        t = v - w;
+        System.out.println("La différence de: " + v + " - " + w + " = " + t);
+
+    }
+}
